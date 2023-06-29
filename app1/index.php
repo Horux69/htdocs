2e0863f4f9ca03ab3b8b8eb6a913d459785e727c
Camilo Castillo <horuxjcc@gmail.com>
2023-06-29 00:29:32 -0500

    first commit

---
+++ app1/index.php
@@ -0,0 +1,106 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+function getDB(){
+    $dbhost = "localhost";
+    $dbname = "app1";
+    $dbuser = "root";
+    $dbpass = "";
+    $mysql_conn_string = "mysql:host=$dbhost;dbname=$dbname";
+    $dbConnection = new PDO($mysql_conn_string, $dbuser, $dbpass);
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConnection;
+}
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/consultaDatos', function ($request, $response, $args) {  //Defino los servicios
+	try{
+		$db =  getDB(); //Carga los datos
+		$sth = $db->prepare("SELECT datId, datNombre, datApellido, datEdad, datDeporte, datImagen, datDescripcion from datos");//Consulta
+		$sth->execute(); //Ejecutamos la consulta
+		$test = $sth->fetchAll(PDO::FETCH_ASSOC);//Guardar los resultados de la consulta
+		//Verificar si se ha cargado algo
+if($test){
+			$response->getBody()->write(json_encode($test)); //write Escribe la respuesta como texto, pero necesito un Json
+			$db = null;//Cerrar la conexion con la base de datos
+		}
+		else{
+			$response->getBody()->write('{"error":"error"}');
+		}
+}catch(PDOException $e){
+			$response->getBody()->write('{"error":{"texto":'.$e->getMessage().'}}'); //En caso que se halla generado algún error
+		}
+    return $response
+    ->withHeader('Content-Type', 'application/json');
+});
+
+$app->post('/createDatos', function ($request, $response, $args) {  //Defino los servicios  $app->post('/updateVeces', function ($request, $response)
+	try{
+		//$data = $request->getParsedBody(); //Recupero los datos  getParams()
+		$json = $request->getBody();
+		$data = json_decode($json, true);
+		$db =  getDB(); //Carga los datos
+		$sth = $db->prepare("INSERT INTO datos (datNombre, datApellido, datEdad, datDeporte, datImagen, datDescripcion) VALUES (?,?,?,?,?,?)");//Insertamos información
+	
+$sth->execute(array($data['datNombre'],$data['datApellido'],$data['datEdad'],$data['datDeporte'],$data['datImagen'],$data['datDescripcion'])); //Sustituimos y ejecutamos la consulta
+		$response->getBody()->write('{"error":"ok"}'); //Cuando la conexión halla terminado
+		
+	}catch(PDOException $e){
+
+			$response->getBody()->write('{"error":{"texto":'.$e->getMessage().'}}'); //En caso que se halla generado algún error
+		}
+    return $response
+    ->withHeader('Content-Type', 'application/json');
+});
+
+// Hacemos la consulta para eliminar datos ->
+
+$app->post('/removeDatos', function ($request, $response, $args) {  //Defino los servicios  $app->post('/updateVeces', function ($request, $response)
+	try{
+		//$data = $request->getParsedBody(); //Recupero los datos  getParams()
+		$json = $request->getBody();
+		$data = json_decode($json, true);
+		$db =  getDB(); //Carga los datos
+		$sth = $db->prepare("DELETE FROM datos WHERE datId = ?");//Insertamos información
+	
+$sth->execute(array($data['datId'])); //Sustituimos y ejecutamos la consulta
+		$response->getBody()->write('{"error":"ok"}'); //Cuando la conexión halla terminado
+		
+	}catch(PDOException $e){
+
+			$response->getBody()->write('{"error":{"texto":'.$e->getMessage().'}}'); //En caso que se halla generado algún error
+		}
+    return $response
+    ->withHeader('Content-Type', 'application/json');
+});
+
+$app->post('/updateDatos', function ($request, $response, $args) {  //Defino los servicios  $app->post('/updateVeces', function ($request, $response)
+	try{
+		//$data = $request->getParsedBody(); //Recupero los datos  getParams()
+		$json = $request->getBody();
+		$data = json_decode($json, true);
+		$db =  getDB(); //Carga los datos
+		$sth = $db->prepare("UPDATE datos SET datNombre = ?, datApellido = ?, datEdad = ?, datDeporte = ?, datImagen = ?, datDescripcion = ? WHERE datId = ?");//Insertamos información
+	
+$sth->execute(array($data['datNombre'],$data['datApellido'],$data['datEdad'],$data['datDeporte'],$data['datImagen'],$data['datId'], $data['datDescripcion'])); //Sustituimos y ejecutamos la consulta
+		$response->getBody()->write('{"error":"ok"}'); //Cuando la conexión halla terminado
+		
+	}catch(PDOException $e){
+
+			$response->getBody()->write('{"error":{"texto":'.$e->getMessage().'}}'); //En caso que se halla generado algún error
+		}
+    return $response
+    ->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
